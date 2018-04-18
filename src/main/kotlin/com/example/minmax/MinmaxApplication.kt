bd30d747e35f90c3c77d3363390431909f7dae27
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-18 16:35:33 +0200

    Init

---
+++ src/main/kotlin/com/example/minmax/MinmaxApplication.kt
@@ -0,0 +1,11 @@
+package com.example.minmax
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinmaxApplication
+
+fun main(args: Array<String>) {
+    runApplication<MinmaxApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/minmax/MinmaxApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.minmax
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class MinmaxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
