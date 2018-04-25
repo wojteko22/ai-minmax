7b0080a7cdb6aac3ecb917814d55e7a417d7dce1
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-25 16:10:11 +0200

    Allow to click field

--- front/src/app/board/board.component.ts
+++ front/src/app/board/board.component.ts
@@ -8,9 +8,16 @@ import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 export class BoardComponent implements OnChanges {
 
   @Input() sideSize: number;
-  side: Array<number>;
+  board: Array<Array<boolean>>;
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.side = Array(this.sideSize);
+    this.board = Array(this.sideSize);
+    for (let i = 0; i < this.board.length; i++) {
+      this.board[i] = Array(this.sideSize).fill(false);
+    }
+  }
+
+  playTurn(row: Array<boolean>, index: number) {
+    row[index] = true;
   }
 }
