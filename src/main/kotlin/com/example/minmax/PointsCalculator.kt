2265bbe495e7fd1e389edd2c43c0bfc867a0bb75
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-06 18:18:31 +0200

    Make vertical test pass

--- src/main/kotlin/com/example/minmax/PointsCalculator.kt
+++ src/main/kotlin/com/example/minmax/PointsCalculator.kt
@@ -2,17 +2,30 @@ package com.example.minmax
 
 class PointsCalculator(data: NewTurn) {
 
-    private val row = data.board[data.rowIndex]
+    private val board = data.board
+    private val row = board[data.rowIndex]
+    private val columnIndex = data.columnIndex
 
     val points: Int
-        get() = horizontalPoints //+ checkVertical() + checkUpperLeftDiagonal() + checkUpperRightDiagonal()
-
+        get() = horizontalPoints + verticalPoints
 
     private val horizontalPoints: Int
         get() {
             var visited = 0
-            for (column in 0 until row.size) {
-                if (!row[column]) {
+            for (columnIndex in 0 until row.size) {
+                if (!row[columnIndex]) {
+                    return 0
+                }
+                visited++
+            }
+            return visited
+        }
+
+    private val verticalPoints: Int
+        get() {
+            var visited = 0
+            for (rowIndex in 0 until row.size) {
+                if (!board[rowIndex][columnIndex]) {
                     return 0
                 }
                 visited++
--- src/test/kotlin/com/example/minmax/PointsCalculatorTest.kt
+++ src/test/kotlin/com/example/minmax/PointsCalculatorTest.kt
@@ -1,14 +1,27 @@
 package com.example.minmax
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 internal class PointsCalculatorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun getPoints() {
         val board = listOf(
                 listOf(true, true),
                 listOf(false, false)
+        )
+        val turn = NewTurn(board, 0, 0)
+        val pointsCalculator = PointsCalculator(turn)
+        val points = pointsCalculator.points
+        assertThat(points).isEqualTo(2)
+    }
+
+    @Test
+    fun getPoints2() {
+        val board = listOf(
+                listOf(true, false),
+                listOf(true, false)
         )
         val turn = NewTurn(board, 0, 0)
         val pointsCalculator = PointsCalculator(turn)
