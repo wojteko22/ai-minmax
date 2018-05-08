6205e3c2948f6f6f6fe02ffcd93b9f4f26f5604a
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-08 10:41:33 +0200

    Refactor front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -15,34 +15,23 @@ export class AppComponent {
   private validators = [Validators.required, Validators.min(2)];
 
   sideSizeControl = new FormControl(4, Validators.compose(this.validators));
-  points = [0, 0];
   allModes = ["human", "consecutive", "points", "alpha-beta"];
   selectedModes = [this.allModes[3], this.allModes[3]];
-  playerIndex = 0;
-  board: Array<Array<boolean>>;
+  myGameState = new GameState(this.sideSizeControl.value);
 
   constructor(private turnService: TurnService) {
     this.sideSizeControl.valueChanges.subscribe(value => this.tryToUpdateSize(value));
-    this.createBoard(this.sideSizeControl.value);
   }
 
   private tryToUpdateSize(size: number) {
     if (this.sideSizeControl.valid) {
-      this.createBoard(size);
-    }
-  }
-
-  private createBoard(size: number) {
-    this.board = Array(size);
-    for (let i = 0; i < this.board.length; i++) {
-      this.board[i] = Array(size).fill(false);
+      this.myGameState.createBoard(size);
     }
   }
 
   onBoardFieldClick(rowIndex: number, columnIndex: number) {
-    this.board[rowIndex][columnIndex] = true;
-    const gameState = new GameState(this.board, this.points, this.playerIndex);
-    const data = new NewMove(gameState, rowIndex, columnIndex);
+    this.myGameState.board[rowIndex][columnIndex] = true;
+    const data = new NewMove(this.myGameState, rowIndex, columnIndex);
     this.turnService.makeAMove(data).subscribe(gameState => this.updateState(gameState));
   }
 
@@ -50,14 +39,15 @@ export class AppComponent {
     if (!gameState) {
       return;
     }
-    this.board = gameState.board;
-    this.points = gameState.points;
-    this.playerIndex = gameState.playerIndex;
-
-    const mode = this.selectedModes[this.playerIndex];
+    this.myGameState = gameState;
+    const mode = this.selectedModes[this.myGameState.playerIndex];
     if (mode != this.allModes[0]) {
       const moveData = new AutoMove(gameState, mode);
       this.turnService.makeAutoMove(moveData).subscribe(gameState => this.updateState(gameState));
     }
+  }
+
+  makeAutoMove() {
+
   }
 }
--- front/src/app/dto/game-state.ts
+++ front/src/app/dto/game-state.ts
@@ -1,8 +1,17 @@
 export class GameState {
-  constructor(
-    public board: Array<Array<boolean>>,
-    public points: Array<number>,
-    public playerIndex: number,
-  ) {
+
+  public board: Array<Array<boolean>>;
+  public points: Array<number> = [0, 0];
+  public playerIndex: number = 0;
+
+  constructor(boardSize: number) {
+    this.createBoard(boardSize);
+  }
+
+  createBoard(size: number) {
+    this.board = Array(size);
+    for (let i = 0; i < this.board.length; i++) {
+      this.board[i] = Array(size).fill(false);
+    }
   }
 }
